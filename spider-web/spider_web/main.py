f354c44691c7e08046e98f4f3af00c20558913fb
Milan Savic <eldumo265@gmail.com>
2023-11-27 16:24:52 +0100

    spider-web

---
+++ spider-web/spider_web/main.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    spider_web_icon = u"\U0001F578"
+    print("################################")
+    print("# SpiderWeb Data Structure! {} #".format(spider_web_icon))
+    print("################################\n")
