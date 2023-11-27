d54a2538c9a6c732a7a29f669e0409c118053e0d
Milan Savic <eldumo265@gmail.com>
2023-11-27 16:30:03 +0100

    Add SpiderWebNode class

---
+++ spider-web/src/spider_web_node.py
@@ -0,0 +1,105 @@
+class SpiderWebNode:
+    def __init__(self, value, prev_node=None, prev_level_node=None):
+        """
+        Constructs a SpiderWebNode with the specified value, previous node, and previous level node.
+
+        :param value: The value to be stored in the node.
+        :param prev_node: Reference to the previous node.
+        :param prev_level_node: Reference to the node on the previous level.
+        """
+        self.value = value
+        self.prev_node = prev_node
+        self.prev_level_node = prev_level_node
+        self.next_node = None
+        self.next_level_node = None
+
+    def get_value(self):
+        """
+        Gets the value stored in the node.
+
+        :return: The value stored in the node.
+        """
+        return self.value
+
+    def set_value(self, value):
+        """
+        Sets the value of the node.
+
+        :param value: The new value to be stored in the node.
+        """
+        self.value = value
+
+    def get_next_node(self):
+        """
+        Gets the reference to the next node.
+
+        :return: The next node.
+        """
+        return self.next_node
+
+    def set_next_node(self, next_node):
+        """
+        Sets the reference to the next node.
+
+        :param next_node: The next node.
+        """
+        self.next_node = next_node
+
+    def get_prev_node(self):
+        """
+        Gets the reference to the previous node.
+
+        :return: The previous node.
+        """
+        return self.prev_node
+
+    def set_prev_node(self, prev_node):
+        """
+        Sets the reference to the previous node.
+
+        :param prev_node: The previous node.
+        """
+        self.prev_node = prev_node
+
+    def get_next_level_node(self):
+        """
+        Gets the reference to the node on the next level.
+
+        :return: The node on the next level.
+        """
+        return self.next_level_node
+
+    def set_next_level_node(self, next_level_node):
+        """
+        Sets the reference to the node on the next level.
+
+        :param next_level_node: The node on the next level.
+        """
+        self.next_level_node = next_level_node
+
+    def get_prev_level_node(self):
+        """
+        Gets the reference to the node on the previous level.
+
+        :return: The node on the previous level.
+        """
+        return self.prev_level_node
+
+    def set_prev_level_node(self, prev_level_node):
+        """
+        Sets the reference to the node on the previous level.
+
+        :param prev_level_node: The node on the previous level.
+        """
+        self.prev_level_node = prev_level_node
+
+    def reset_pointers(self):
+        """Resets the pointers of the SpiderWebNode, setting all references to None."""
+        self.prev_node = None
+        self.next_node = None
+        self.prev_level_node = None
+        self.next_level_node = None
+
+    def __str__(self):
+        """Returns a string representation of the SpiderWebNode, including its value."""
+        return f"SpiderWebNode{{value={self.value}}}"
